c7d76e72a2dfab4bcc913e662b60e7bf55d4568e
Kamruz Zaman <kamruz.zaman8991@gmail.com>
2026-06-07 00:03:08 +0600

    Step 1: Replace Tailwind with Bootstrap, update layout with Poppins font and Bootstrap JS client component

---
+++ app/BootstrapClient.js
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function BootstrapClient({ children }) {
+  useEffect(() => {
+    // Dynamic import of Bootstrap JS bundle
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+
+  return <>{children}</>;
+}
